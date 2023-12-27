e8f2700f3a32fe23717b785f032cac7f8ff6f0a0
Hasmuddin <hasmuddin97175@gmail.com>
2023-12-27 17:44:33 +0530

    first commit

---
+++ server/database/mongoose.js
@@ -0,0 +1,31 @@
+// Require the library
+const mongoose = require('mongoose');
+
+// Here we are using the MongoDB URL (i.e., MongoDB Atlas)
+// const db =process.env.MONGOOSELINK;
+
+// Create an async function to handle the connection
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect("mongodb+srv://hasmuddin97175:nLXf0wvxECBrOy8S@cluster0.yvcxxrt.mongodb.net/user?retryWrites=true&w=majority", {
+
+        });
+        console.log('Successfully connected to the database');
+    } catch (error) {
+        console.error('Failed to connect to the database', error);
+    }
+};
+
+// Call the async function to connect to the database
+connectToDatabase();
+
+
+module.exports = mongoose.connetcion;
+
+
+
+// id ajayagrawal728
+// password y51gLBiA7QMkzKk6
+
+// MONGO_URL ='mongodb+srv://hasmuddin97175:X8mByhvnmc1USy77@cluster0.wwptqgh.mongodb.net/todo?retryWrites=true&w=majority'
+
